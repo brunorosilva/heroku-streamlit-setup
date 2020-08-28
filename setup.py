e24a9d923e84f38fc6740a7a2578b0dc0c0fc9c6
Bruno Rodrigues Silva <b.rosilva1@gmail.com>
2020-08-28 01:05:31 -0300

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,10 +1,30 @@
 from setuptools import setup
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+    
 setup(
-    name = 'heroku_streamlit_cli',
-    version = '0.1.0',
-    packages = ['hss'],
+    name = 'hss',
+    version = '0.0.2',
+    author='Bruno Rodrigues Silva',
+    author_email='rodriguessilvabruno@outlook.com',
+    description="command line interface for setting up streamlit webapps on heroku",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url = 'https://github.com/brunorosilva/heroku-streamlit-setup',
+    packages = setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     entry_points = {
         'console_scripts': [
             'hss = hss.__main__:main'
-        ]
-    })
+        ],
+    },
+    python_requires='>=3.6',
+    install_requires=["pipreqs"]
+)
